120ab8184b93f567387b0b5037c238add1adbcaa
Doryan <71377447+denisdoryan@users.noreply.github.com>
2021-02-04 16:51:58 +0100

    Update traitement.php

--- traitement.php
+++ traitement.php
@@ -1,3 +1,4 @@
 <?php
-	echo 'Vous vous appelez '.$_POST['Prenom'].' '.$_POST['Nom2'].' vous avez '.$_POST['Age'].' ans.';
+	echo '<h1>Vous vous appelez '.$_POST['Prenom'].' '.$_POST['Nom2'].' vous avez '.$_POST['Age'].' ans.</h1>';
+	echo '<h1>Vous êtes en classe de '.$_POST['Classe'].' '.$_POST['Serie'].'.</h1>';
 ?>
